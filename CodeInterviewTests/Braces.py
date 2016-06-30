27c0f2cde00def355636c13940f08540a59fd5f7
Han <chengbohan@gmail.com>
2016-06-30 19:27:19 -0400

    practice with list, stack, dict,

---
+++ CodeInterviewTests/Braces.py
@@ -0,0 +1,23 @@
+
+values = ["[](){}", "[(){([])}[()]", "[([)]"]
+
+
+braceMap = { "[" : "]","(" : ")","{" : "}",
+"]" : "NoMatch",")" : "NoMatch","}" : "NoMatch"
+             }
+
+
+for val in values:
+    stack = []
+    for char in val:
+    # for i in range(len(val)):
+    #     char = val[i]
+        if len(stack) != 0 and char == braceMap[ stack[len(stack)-1] ] :
+            stack.pop()
+        else:
+            stack.append(char)
+    if len(stack)==0 :
+        print "Yes",
+    else:
+        print "No",
+
