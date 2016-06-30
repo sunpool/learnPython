aba24ec0d692992f5442c6980d24f542bcf3b216
Han <chengbohan@gmail.com>
2016-06-30 18:38:13 -0400

    native python 2.7 print. Compose string before print

--- CodeInterviewTests/Compress.py
+++ CodeInterviewTests/Compress.py
@@ -1,9 +1,25 @@
 
-from __future__ import print_function
+# from __future__ import print_function
 
 a = ["aaabbbbbkkh", "cceedfg"]
 
 # print a;
+#
+# for word in a:
+#     tmp = ""
+#     num = 1
+#     for char in word:
+#         if tmp == char:
+#             num += 1
+#         else:
+#             if num > 1:
+#                 print ( num, end="" )
+#                 num = 1
+#             tmp = char
+#             print( char, end = "" )
+#     print( "" )
+
+string = ""
 
 for word in a:
     tmp = ""
@@ -13,9 +29,9 @@ for word in a:
             num += 1
         else:
             if num > 1:
-                print ( num, end="" )
+                string += str(num)
                 num = 1
             tmp = char
-            print( char, end = "" )
-    print( "" )
+            string += char
 
+print string
