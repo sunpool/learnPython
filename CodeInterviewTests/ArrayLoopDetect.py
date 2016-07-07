997f4958d2ae7d39fbd6a50a496c706d3816cd3e
Han <chengbohan@gmail.com>
2016-07-07 19:27:55 -0400

    chasing pointer solution to loop detection in single linked list

--- CodeInterviewTests/ArrayLoopDetect.py
+++ CodeInterviewTests/ArrayLoopDetect.py
@@ -8,9 +8,10 @@ shuffle(a)
 print a
 print range(20)
 
+
 # DFS?  -> extra memory version
 def loop_detect_w_extra_memory(a):
-    visited = [False]*len(a)
+    visited = [False] * len(a)
 
     i = 0
     el = a[0]
@@ -23,18 +24,22 @@ def loop_detect_w_extra_memory(a):
             i = el
             el = a[el]
 
-# loop_detect_w_extra_memory(a)
+
+loop_detect_w_extra_memory(a)
+
 
 # 2 chasing pointers
 def move_p(a, p):
     if p < 0 or p >= len(a):
-        raise ValueError( "invalid input array" )
+        raise ValueError("invalid input array")
     return a[p]
+
 
 p1 = p2 = 0
 while True:
     p1 = move_p(a, p1)
     p2 = move_p(a, move_p(a, p2))
+    print p1, p2
     if p1 == p2:
-        print "Loop detected"
+        print "Loop detected at ", p1
         break
