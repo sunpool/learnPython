cd3139a2f00bcc591b28ec9b9c33020987cb1179
Han <chengbohan@gmail.com>
2016-07-08 18:25:52 -0400

    chasing pointer will always meet at index 0. you can prove it.

--- CodeInterviewTests/ArrayLoopDetect.py
+++ CodeInterviewTests/ArrayLoopDetect.py
@@ -3,11 +3,12 @@
 from random import shuffle
 
 # shuffled range array, must have loop
-a = range(20)
+a = range(21)
 shuffle(a)
 print a
 print range(20)
 
+# a = [10, 15, 11, 6, 19, 12, 4, 2, 18, 5, 7, 8, 16, 17, 13, 9, 14, 3, 0, 1]
 
 # DFS?  -> extra memory version
 def loop_detect_w_extra_memory(a):
@@ -34,12 +35,15 @@ def move_p(a, p):
         raise ValueError("invalid input array")
     return a[p]
 
+# for a chasing pointer problem, every time those 2 pt will meet at the index 0
+def chasing_pointer(a):
+    p1 = p2 = 0
+    while True:
+        p1 = move_p(a, p1)
+        p2 = move_p(a, move_p(a, p2))
+        print p1, p2
+        if p1 == p2:
+            print "Loop detected at ", p1
+            break
 
-p1 = p2 = 0
-while True:
-    p1 = move_p(a, p1)
-    p2 = move_p(a, move_p(a, p2))
-    print p1, p2
-    if p1 == p2:
-        print "Loop detected at ", p1
-        break
+chasing_pointer(a)
