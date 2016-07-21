7909b8885b91865b6650d60c5e2570d09cb48eb8
Han <chengbohan@gmail.com>
2016-07-21 08:54:53 -0400

    fix symmetric matrix value get

--- CodeInterviewTests/FriendCircle.py
+++ CodeInterviewTests/FriendCircle.py
@@ -1,30 +1,35 @@
-
 dim = 5
 
 # non directional graph connection is symmetric
 non_directional = ["YYYXX", "YXXY", "XYY", "XY", "X"]
 dim = len(non_directional)
 
-def getValue_symetric_matrix(i,j, matrix= non_directional):
+
+def getValue_symetric_matrix(i, j, matrix=non_directional):
+    print "input", i, j
     if i <= j:
         row = i
-        col = j
+        col = j - i
     else:
         row = j
         col = i
+    print "getVa", row, col
+    print
     return matrix[row][col]
+
 
 # print out all circles in graph,
 # visited = [[False]*dim]*dim
 # visited = [[False for i in xrange(dim)] for j in xrange(dim)]
 
 # python 3+ xrange is off, and range become xrange that without instantiate a list. can be forced to list as list(range(10))
-visited = [False]*dim
+visited = [False] * dim
 # visited = [False for i in xrange(dim)]
 
 # loop DFS
 stack = [0]
 visited[0] = True
 for i in xrange(dim):
-
-
+    print i
+    connected = [j for j in xrange(dim) if getValue_symetric_matrix(i, j) == "Y"]
+    print connected
