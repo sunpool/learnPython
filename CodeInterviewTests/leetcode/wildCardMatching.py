589b65f94d52b30c01c0edb1394fb42822b2a356
Han <chengbohan@gmail.com>
2016-08-09 18:52:17 -0400

    add Dynamic programming with optimized memory

--- CodeInterviewTests/leetcode/wildCardMatching.py
+++ CodeInterviewTests/leetcode/wildCardMatching.py
@@ -1,11 +1,26 @@
-
-target = "afbcacbcc"
+target = "afbcacb"
 pat = "a*acb"
+
 # Dynamic programming
-# sol = list[]
+sol = [False] * (len(target) + 1)
+sol[0] = True
+for ip, pStr in enumerate(pat):
+    if pStr == "*":
+        for it in xrange(1, len(target)):
+            sol[it] = sol[it - 1] or sol[it]
+    else:
+        # for it, tStr in enumerate(target):
+        for it in xrange(len(target), 0, -1):
+            if target[it - 1] == pStr or pStr == "?":
+                sol[it] = sol[it - 1]
+            else:
+                sol[it] = False
+
+print sol[len(target)]
+
 
 # quick switches
-def wildcardMatching( target, pat ):
+def wildcardMatching(target, pat):
     # match = True
     match_target_id, match_pattern_id = -1, -1
 
@@ -15,14 +30,14 @@ def wildcardMatching( target, pat ):
         t = target[it]
 
         if ip < len(pat) and (t == pat[ip] or pat[ip] == "?"):
-            it += 1     # python does not have operator ++ -- ?
+            it += 1  # python does not have operator ++ -- ?
             ip += 1
         elif ip < len(pat) and pat[ip] == "*":
             match_target_id, match_pattern_id = it, ip
             ip += 1
         elif match_target_id != -1:
-            match_target_id += 1        # forward by only 1 step
-            ip = match_pattern_id + 1   # reset to last * position + 1
+            match_target_id += 1  # forward by only 1 step
+            ip = match_pattern_id + 1  # reset to last * position + 1
             it = match_target_id
         else:
             return False
@@ -33,8 +48,5 @@ def wildcardMatching( target, pat ):
     print ip == len(pat)
     return ip == len(pat)
 
-target = "afbcacbcacb"
-pat = "a*acb"
-print len(pat)
 
-wildcardMatching( target, pat )
+wildcardMatching(target, pat)
