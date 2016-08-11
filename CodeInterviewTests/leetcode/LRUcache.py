8993264594db12f0cfb30506e6f08794d984c30b
Han <chengbohan@gmail.com>
2016-08-10 22:19:23 -0400

    LRU cache, heapq implementation
    mixed usage of heap priority queue, and dictionary, bridged as python container always holds reference, so heap entry can be looked up through dict().
    
    Findings:
    1. python use reference a lot. Even string in array is referenced.
    e.g. line 66 and 67, {keyX: [ .., keyX, ... ]}, when I get val through keyX, and set val[1] -> "Removed Entry", the variable keyX used to fetch val also changed to "Removed Entry".
    -> I don't think python is good for very serious infra
    
    2. Priority queue with heap implementation, is good idea but not good usage.
    -> because Heap cannot update priority easily. Common way is to mark as deleted and push.

--- CodeInterviewTests/leetcode/LRUcache.py
+++ CodeInterviewTests/leetcode/LRUcache.py
@@ -1,3 +1,4 @@
+# plain dict implementation
 class LRU_cache():
     def __init__(self, capacity=50):
         self.store = {}
@@ -13,13 +14,14 @@ class LRU_cache():
 
     def set(self, key, val):
         if len(self.store) == self.capacity:
-            del_key = min(self.sore, key=self.store.get)  # nlogn, not optimal
+            del_key = min(self.store, key=self.store.get)  # nlogn, not optimal
             del self.store[del_key]
 
         self.store[key] = val
-        self.LRU_count.setdefault(key,0)
+        self.LRU_count.setdefault(key, 0)
         self.LRU_count[key] += 1
         return self.store[key]
+
 
 LRU = LRU_cache()
 
@@ -27,4 +29,74 @@ print LRU.get("hey ")
 
 print LRU.set("ha", 20)
 
-print  LRU.get("ha"), LRU,
+print LRU.get("ha"), LRU
+
+# use heapq
+#   drawback: internal storage can go beyond capacity, as the deleted ones are only marked as dead
+#             instead of being really deleted
+import heapq
+
+
+# import itertools
+
+class LRU_heap():
+    REMOVED = "<Removed Entry>"
+
+    def __init__(self, capacity):
+        self.heap = []
+        self.store = {}
+        self.capacity = capacity
+        # self.counter = itertools.count()
+
+    def set(self, key, val):
+        count = 0
+        if key in self.store:
+            count = self.store[key][0]
+            self.__delete_old_keyVal(key)
+        elif len(self.store) == self.capacity:
+            self.__pop_LR()
+
+        entry = [count + 1, key, val]
+        self.store[key] = entry
+        heapq.heappush(self.heap, entry)
+        return self.store[key][-1]
+
+    def __delete_old_keyVal(self, key):
+        entry = self.store[key]
+        del self.store[key]
+        entry[1] = self.REMOVED
+
+    def __pop_LR(self):
+        key = self.REMOVED
+        while key == self.REMOVED:
+            count, key, val = heapq.heappop(self.heap)
+        del self.store[key]
+
+    def get(self, key):
+        if key in self.store:
+            count, key1, val = self.store[key]
+            self.__delete_old_keyVal(key)
+            entry = [count + 1, key, val]
+            self.store[key] = entry
+            heapq.heappush(self.heap, entry)
+            return self.store.get(key)
+
+        raise KeyError("no such key")
+
+LRU_cache_heap = LRU_heap(2)
+
+# LRU_cache_heap = LRU_cache(2)
+
+print "Heap implementation: "
+try:
+    print LRU_cache_heap.get("a")
+except KeyError as ke:
+    print "no such key warning: ", ke
+
+print LRU_cache_heap.set("x", 20), LRU_cache_heap.get('x')
+print LRU_cache_heap.set("x", 1),  LRU_cache_heap.set("y", 1)
+print LRU_cache_heap.store
+print LRU_cache_heap.set("z", 1)
+print LRU_cache_heap.store
+
+
