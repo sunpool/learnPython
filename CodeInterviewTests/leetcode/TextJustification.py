cd588a10fd3798734a4427a163439c3626bb398b
Han <chengbohan@gmail.com>
2016-08-17 19:24:44 -0400

    formatting

--- CodeInterviewTests/leetcode/TextJustification.py
+++ CodeInterviewTests/leetcode/TextJustification.py
@@ -67,7 +67,7 @@ for i, line in enumerate(lines[:len(lines) - 1]):
 
 lastLine = lines[len(lines) - 1]
 strLastLine = reduce(lambda el, rdc: rdc + " " + el, lastLine[1:], lastLine[0])
-strLastLine += " "*(L-len(strLastLine))
-strLines.append( strLastLine )
+strLastLine += " " * (L - len(strLastLine))
+strLines.append(strLastLine)
 
 print strLines
