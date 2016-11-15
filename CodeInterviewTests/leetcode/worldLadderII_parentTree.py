fae590787bedd6447c9e33268e7786d7aff7e164
Han <chengbohan@gmail.com>
2016-11-15 08:28:45 -0500

    1. remove global variable, 2. don't need to load leaves when backward DFS. Use a good init acc, and endword as leave 3. in case begin word is in word list, don't load it on path 4. add check when there is no path.

--- CodeInterviewTests/leetcode/worldLadderII_parentTree.py
+++ CodeInterviewTests/leetcode/worldLadderII_parentTree.py
@@ -1,29 +1,63 @@
+# https://leetcode.com/problems/word-ladder-ii/
+
+# Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+#
+# Only one letter can be changed at a time
+# Each intermediate word must exist in the word list
+# For example,
+#
+# Given:
+# beginWord = "hit"
+# endWord = "cog"
+# wordList = ["hot","dot","dog","lot","log"]
+# Return
+#   [
+#     ["hit","hot","dot","dog","cog"],
+#     ["hit","hot","lot","log","cog"]
+#   ]
+# Note:
+# All words have the same length.
+# All words contain only lowercase alphabetic characters.
+
+
+
+# SOLUTION
+
 from collections import deque
 
 beginWord = "hit"
 endWord = "cog"
 wordList = ["hot", "dot", "dog", "hox", "lot", "log"]
-wordSet = set(wordList)
 
-childDict = dict()  # implementation 1
-parentDict = dict()  # implementation 2
+# beginWord = "hot"
+# endWord = "dog"
+# wordList = ["hot", "dog"]
+
+# beginWord = "h"
+# endWord = "d"
+# wordList = ["b", "c"]
+
+
+wordSet = set(wordList)
 
 
 # same as
-#   mymay.setdefault(key, default)
+#   mymap.setdefault(key, default)
 #   use python native implementation.
+#
+# def component_ensure(mymap, key, default):
+#     if key not in mymap:
+#         mymap[key] = default
+#     return mymap[key]
 
-def component_ensure(mymay, key, default):
-    if key not in mymay:
-        mymay[key] = default
-    return mymay[key]
 
-
-# def BFS_build( beginWord, endWord, wordSet = wordSet, childDict ):
 def searchNextLevel(children, endWord, wordSet, parentDict):
     stop = False
     gChildren = set()
-    hitword = set()
+    # hitword = set()   // remove leave node record, as improved DFS tree travers
+
+    # don't load identical word on path
+    wordSet -= set(children)
 
     for word in children:
         for i, char in enumerate(word):
@@ -32,50 +66,44 @@ def searchNextLevel(children, endWord, wordSet, parentDict):
                 if nWord == endWord:
                     stop = True
 
-                    # component_ensure(parentDict, nWord, [])
                     parentDict.setdefault(nWord, [])
                     parentDict[nWord].append(word)
-
-                    hitword.add(word)
+                    # hitword.add(word)
 
                 if nWord in wordSet:
                     # wordSet.remove(nWord)   # wrong! This will remove other equal depth paths, should do after this level is done
-
-                    # component_ensure(parentDict, nWord, [])
                     parentDict.setdefault(nWord, [])
                     parentDict[nWord].append(word)
-
                     gChildren.add(nWord)
 
-    # for word in gChildren:  # remove redundant after this level is done
-    #     wordSet.remove(word)
+    if len(gChildren) == 0 and not stop:
+        return set()
+
+    # flag the word in wordSet as being loaded in tree.
     wordSet = wordSet - gChildren
 
     if not stop:
-        return searchNextLevel(gChildren, endWord, wordSet, childDict)
-    else:
-        return hitword
+        return searchNextLevel(gChildren, endWord, wordSet, parentDict)
 
 
 # beginWord scoped
-def DFS_travers(leave, acc):  # , beginword):
-    if leave == beginWord:
-        print [leave] + acc
-        return
-        # return leave
+def DFS_travers(leave, acc, parentDict):
+    new_acc = []
+    for a in acc:
+        new_acc.append([leave] + a)
 
-    # parents = end_word_tree[leave]  # will throw if not exist
-    # parents = end_word_tree.get(leave, [])
+    if leave == beginWord:
+        return new_acc
+
     parents = parentDict.get(leave, [])
 
+    paths = []
     for par in parents:
-        DFS_travers(par, [leave] + acc)
-        # print leave
-
+        paths = paths + DFS_travers(par, new_acc, parentDict)
+    return paths
 
 
 begin_word_tree = dict()
 leaveWords = searchNextLevel([beginWord], endWord, set(wordList), begin_word_tree)
-
-for leave in leaveWords:
-    DFS_travers(leave, ["cog"])
+ret = DFS_travers(endWord, [[]], begin_word_tree)
+print ret
