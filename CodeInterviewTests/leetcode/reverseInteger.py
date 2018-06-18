4136ffb6692b8e575ab2e95457cff013b3d7501e
Han <chengbohan@gmail.com>
2018-06-18 06:47:54 -0400

    revert int (easy) 1. pending python Max 2. python 2 does not have parameter annotation

---
+++ CodeInterviewTests/leetcode/reverseInteger.py
@@ -0,0 +1,26 @@
+def revertInt(x):
+    '''
+    :param x: int
+    :return: int
+    '''
+    ret = 0
+    Int_Max = 1e12
+
+    while x != 0:
+        pop = x % 10
+        x /= 10
+        if ret * 10 > Int_Max or ( ret * 10 == Int_Max and pop > 7 )
+            return 0
+        if ret * 10 < Int_Min or (ret * 10 == Int_min and pop < -8 )
+            return 0
+        ret = ret * 10 + pop
+
+
+# def revertInt2(x):
+#     if x < 0:
+#         return -revertInt2(-x)
+#     for(;x != 0; x/=10):
+#         d = x % 10
+#         if d < 8:
+#             if x > Int_Max
+#         if x > (d < 8 ? Int_Max : Int_Min):
