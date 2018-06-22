6eb0675d16348d359cab3c3cc948eb87a14470fa
Han <chengbohan@gmail.com>
2018-06-22 07:29:54 -0400

    update with python 3.6

--- CodeInterviewTests/Braces.py
+++ CodeInterviewTests/Braces.py
@@ -1,23 +1,19 @@
-
 values = ["[](){}", "[(){([])}[()]", "[([)]"]
 
-
-braceMap = { "[" : "]","(" : ")","{" : "}",
-"]" : "NoMatch",")" : "NoMatch","}" : "NoMatch"
-             }
-
+braceMap = {"[": "]", "(": ")", "{": "}",
+            "]": "NoMatch", ")": "NoMatch", "}": "NoMatch"
+            }
 
 for val in values:
     stack = []
     for char in val:
-    # for i in range(len(val)):
-    #     char = val[i]
-        if len(stack) != 0 and char == braceMap[ stack[len(stack)-1] ] :
+        # for i in range(len(val)):
+        #     char = val[i]
+        if len(stack) != 0 and char == braceMap[stack[len(stack) - 1]]:
             stack.pop()
         else:
             stack.append(char)
-    if len(stack)==0 :
-        print "Yes",
+    if len(stack) == 0:
+        print("Yes", )
     else:
-        print "No",
-
+        print("No", )
